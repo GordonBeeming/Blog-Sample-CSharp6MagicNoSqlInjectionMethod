2f065b139842f274e64e498a51773f8bae780275
Gordon Beeming <gordon@beeming.co.za>
2017-02-09 21:43:05 +0200

    writing out the parameters as well

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,18 +19,24 @@ SELECT *
 FROM dbo.tb_People 
 WHERE Name = {name} AND Surname = {surname}", sqlConnection);
             Console.WriteLine(theCommand.CommandText);
+            Console.WriteLine();
+            for (int index = 0; index < theCommand.Parameters.Count; index++)
+            {
+                Console.WriteLine($"{theCommand.Parameters[index].ParameterName} = {theCommand.Parameters[index].Value}");
+            }
+            Console.WriteLine();
         }
 
         public static SqlCommand ToSqlCommand(FormattableString formattableString, SqlConnection sqlConnection)
         {
             var args = formattableString.GetArguments();
-            var sql = string.Format(CultureInfo.InvariantCulture, 
-                                        formattableString.Format, 
+            var sql = string.Format(CultureInfo.InvariantCulture,
+                                        formattableString.Format,
                                         Enumerable.Range(0, args.Length)
                                                     .Select(index => $"@p{index}")
                                                     .ToArray());
             var command = new SqlCommand(sql, sqlConnection);
-            for(int index = 0; index < args.Length;index++)
+            for (int index = 0; index < args.Length; index++)
             {
                 command.Parameters.AddWithValue($"@p{index}", args[index]);
             }
